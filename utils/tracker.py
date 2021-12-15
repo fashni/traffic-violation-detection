5dab7a1937a0672aef996b56126a59b171e77f70
fashni <fashni@outlook.com>
2021-12-15 17:01:29 +0700

    add output table and save output

--- utils/tracker.py
+++ utils/tracker.py
@@ -1,6 +1,14 @@
-import numpy as np
-from .utils import is_crossing_stop_line
+import json
+from datetime import datetime
+from pathlib import Path
 
+import cv2
+import numpy as np
+from .utils import is_crossing_stop_line, increment_path
+
+BASE_DIR = Path().resolve()
+RUNS_DIR = BASE_DIR / 'runs'
+RUNS_DIR.mkdir(parents=True, exist_ok=True)
 
 class EuclideanDistTracker:
   def __init__(self, thres=25):
@@ -52,74 +60,118 @@ class EuclideanDistTracker:
     self.center_points = new_center_points.copy()
     return np.array(objects_bbs_ids)
 
+  def destroy(self):
+    self.center_points = {}
+    self.id_count = 0
+
 
 class CarRecord:
-  def __init__(self):
+  JPEG_QUALITY = [cv2.IMWRITE_JPEG_QUALITY, 100]
+
+  def __init__(self, nosave=False):
     # store objects in dictionary
     self.records = {}
+    self.nosave = nosave
+    # self.create_run()
 
-  def update(self, cboxes_id, c_imgs, ws_imgs): # bbox car [x1, y1, x2, y2, id, stop_line, seat_belt_n]
-    self.update_box(cboxes_id)
-    self.update_car_imgs(c_imgs)
-    self.update_ws_imgs(c_imgs)
+  def create_run(self):
+    exec_time = datetime.now()
+    RUN_DIR = RUNS_DIR / exec_time.isoformat()
+    RUN_DIR.mkdir(parents=True, exist_ok=True)
+    FRAME_DIR = RUN_DIR / 'frames'
+    FRAME_DIR.mkdir(parents=True, exist_ok=True)
+    CAR_DIR = RUN_DIR / 'cars'
+    CAR_DIR.mkdir(parents=True, exist_ok=True)
+    WS_DIR = RUN_DIR / 'windshields'
+    WS_DIR.mkdir(parents=True, exist_ok=True)
+    JSON_DIR = RUN_DIR / 'results.json'
+    self.__dict__.update(locals())
+
+  def update(self, frame_id, cboxes_id, ws_imgs, cw_res): # bbox car [x1, y1, x2, y2, id, stop_line, seat_belt_n]
+    self.update_box(cboxes_id, frame_id)
+    self.save_json()
+    if not self.nosave:
+      self.update_ws_imgs(ws_imgs)
+      self.save_frame(cw_res)
+      self.update_car_imgs(cboxes_id, cw_res.img)
     return self.records
 
-  def update_box(self, cboxes_id):
+  def update_box(self, cboxes_id, frame_id):
     for cbox in cboxes_id:
       object_id = int(cbox[4])
       box = cbox[:4]
       stop_line = not not cbox[5]
-      n_seat_belt = int(cbox[6])
+      n_passenger = int(cbox[6])
+      n_seat_belt = int(cbox[7])
 
       # create first instance for newly encountered object
       if object_id not in self.records.keys():
         self.records[object_id] = {
-          'positions': [box],
-          'stop_line': stop_line,
-          'n_seat_belt': n_seat_belt
+          'frame_id': [frame_id],
+          'positions': [box.tolist()],
+          'stop_line': [stop_line],
+          'n_passenger': [n_passenger],
+          'n_seat_belt': [n_seat_belt]
         }
         continue
 
       # for new instance of saved object
       # check if object has each key, then append
-      if 'positions' in self.records[object_id].keys():
-        self.records[object_id]['positions'] += [box]
+      if 'frame_id' in self.records[object_id].keys():
+        self.records[object_id]['frame_id'] += [frame_id]
       else: # else add new key
-        self.records[object_id]['positions'] = [box]
+        self.records[object_id]['frame_id'] = [frame_id]
+      if 'positions' in self.records[object_id].keys():
+        self.records[object_id]['positions'] += [box.tolist()]
+      else: # else add new key
+        self.records[object_id]['positions'] = [box.tolist()]
       if 'stop_line' in self.records[object_id].keys():
-        self.records[object_id]['stop_line'] |= stop_line
+        self.records[object_id]['stop_line'] += [stop_line]
       else:
         self.records[object_id]['stop_line'] = stop_line
+      if 'n_passenger' in self.records[object_id].keys():
+        self.records[object_id]['n_passenger'] += [n_passenger]
+      else:
+        self.records[object_id]['n_passenger'] = [n_passenger]
       if 'n_seat_belt' in self.records[object_id].keys():
-        self.records[object_id]['n_seat_belt'] = max(self.records[object_id]['n_seat_belt'], n_seat_belt)
+        self.records[object_id]['n_seat_belt'] += [n_seat_belt]
       else:
-        self.records[object_id]['n_seat_belt'] = n_seat_belt
+        self.records[object_id]['n_seat_belt'] = [n_seat_belt]
 
-  def update_car_imgs(self, c_imgs):
-    for idx, c_img in c_imgs:
-      if idx not in self.records.keys():
-        self.records[idx] = {'car_imgs': [c_img]}
-        continue
+  def update_car_imgs(self, cboxes_id, frame):
+    for cbox in cboxes_id:
+      idx = int(cbox[4])
+      x1,y1,x2,y2 = cbox[:4].astype(int)
+      c_img = frame[y1:y2, x1:x2, :]
+      car_img_dir = self.CAR_DIR / str(idx)
+      car_img_dir.mkdir(parents=True, exist_ok=True)
+      file_path = increment_path(car_img_dir / f'{idx}_car.jpg')
+      cv2.imwrite(str(file_path), c_img, self.JPEG_QUALITY)
 
-      if 'car_imgs' not in self.records[idx].keys():
-        self.records[idx]['car_imgs'] = [c_img]
-      else:
-        self.records[idx]['car_imgs'] += [c_img]
-      
   def update_ws_imgs(self, ws_imgs):
     for idx, ws_img in ws_imgs:
-      if idx not in self.records.keys():
-        self.records[idx] = {'windshield_imgs': [ws_img]}
-        continue
+      ws_img_dir = self.WS_DIR / str(idx)
+      ws_img_dir.mkdir(parents=True, exist_ok=True)
+      file_path = increment_path(ws_img_dir / f'{idx}_windshield.jpg')
+      cv2.imwrite(str(file_path), ws_img.img, self.JPEG_QUALITY)
+      with file_path.with_suffix('.txt').open(mode='wt') as f:
+        if ws_img.yolo.size > 0:
+          boxes, cls_ids = ws_img.yolo[:, :4], ws_img.yolo[:, 5].astype(int)
+          [f.write(f'{cls_id} ' + ' '.join(box) + '\n') for box, cls_id in zip(boxes.astype(str), cls_ids.astype(str))]
 
-      if 'windshield_imgs' not in self.records[idx].keys():
-        self.records[idx]['windshield_imgs'] = [ws_img]
-      else:
-        self.records[idx]['windshield_imgs'] += [ws_img]
+  def save_frame(self, res):
+    file_path = increment_path(self.FRAME_DIR / f'frame.jpg')
+    cv2.imwrite(str(file_path), res.img)
+    with file_path.with_suffix('.txt').open(mode='wt') as f:
+      if res.yolo.size > 0:
+        boxes, cls_ids = res.yolo[:, :4], res.yolo[:, 5].astype(int)
+        [f.write(f'{cls_id} ' + ' '.join(box) + '\n') for box, cls_id in zip(boxes.astype(str), cls_ids.astype(str))]
 
-  def save(self):
-    pass
+  def save_json(self):
+    with open(self.JSON_DIR, 'w') as f:
+      json.dump(self.records, f, indent=4)
 
   def destroy(self):
     del self.records
     self.records = {}
+    self.create_run()
--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,8 @@
+import glob
+import re
 import random
 from functools import lru_cache
+from pathlib import Path
 
 import cv2
 import numpy as np
@@ -177,3 +180,17 @@ def is_crossing_stop_line(bbox_id, line_pos):
     crossed.append(cross)
 
   return np.array(crossed)
+
+def increment_path(path, exist_ok=False, sep='', mkdir=False):
+  # Increment file or directory path, i.e. runs/exp --> runs/exp{sep}2, runs/exp{sep}3, ... etc.
+  path = Path(path)
+  if path.exists() and not exist_ok:
+    path, suffix = (path.with_suffix(''), path.suffix) if path.is_file() else (path, '')
+    dirs = glob.glob(f"{path}{sep}*")  # similar paths
+    matches = [re.search(rf"%s{sep}(\d+)" % path.stem, d) for d in dirs]
+    i = [int(m.groups()[0]) for m in matches if m]  # indices
+    n = max(i) + 1 if i else 1  # increment number
+    path = Path(f"{path}{sep}{n}{suffix}")  # increment path
+  if mkdir:
+    path.mkdir(parents=True, exist_ok=True)  # make directory
+  return path
